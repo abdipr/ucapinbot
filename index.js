e4b5f9d35bd0308ac7bc19d9dcda9bf965ef4d60
Abdi Putrana Radian <57528253+abdipr@users.noreply.github.com>
2024-09-20 10:04:30 +0000

    e

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const { AoiClient } = require("aoi.js");
+const { AoiClient } = require('aoi.js');
 
-const client = new AoiClient({
-    token: "MTI4NjYxMzIwNTY3Mzg0MDY4MQ.GN-L6Y.nvSgF-UNySdDFpX3Py3iDT9I7SLD_3gX7XZyAM", // Here goes the Token you copied earlier!
-    prefix: ".", // Here goes the prefix you want to use for your bot!
-    intents: ["MessageContent", "Guilds", "GuildMessages"],
-    events: ["onMessage", "onInteractionCreate"]
+const bot = new AoiClient({
+    token: "MTI4NjYxMzIwNTY3Mzg0MDY4MQ.GN-L6Y.nvSgF-UNySdDFpX3Py3iDT9I7SLD_3gX7XZyAM", // Gantilah dengan variabel environment jika menggunakan Vercel
+    prefix: ".", // Gantilah sesuai kebutuhan
 });
 
-// Ping Command which responds when doing "!ping"
-client.command({
+// Event saat bot siap
+bot.onReady(() => {
+    console.log("Bot is online!");
+});
+bot.loadCommands("./commands");
+
+// Contoh command
+bot.command({
     name: "ping",
-    code: `Pong! $pingms`
+    code: `Pong!`
 });
 
-client.loadCommands("./commands");
-console.log('Bot online!');
+// Menjalankan bot
+bot.start();
