1b2fd9e6a6461fd779c51717f2a7e17ced80921f
Abdi Putrana Radian <57528253+abdipr@users.noreply.github.com>
2024-09-20 10:07:19 +0000

    e

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const { AoiClient } = require('aoi.js');
 
 const bot = new AoiClient({
-    token: "MTI4NjYxMzIwNTY3Mzg0MDY4MQ.GN-L6Y.nvSgF-UNySdDFpX3Py3iDT9I7SLD_3gX7XZyAM", // Gantilah dengan variabel environment jika menggunakan Vercel
-    prefix: ".", // Gantilah sesuai kebutuhan
+    token: "MTI4NjYxMzIwNTY3Mzg0MDY4MQ.GN-L6Y.nvSgF-UNySdDFpX3Py3iDT9I7SLD_3gX7XZyAM",
+    prefix: ".",
 });
 
-// Event saat bot siap
 bot.onReady(() => {
     console.log("Bot is online!");
 });
 bot.loadCommands("./commands");
 
-// Contoh command
 bot.command({
     name: "ping",
     code: `Pong!`
 });
 
-// Menjalankan bot
 bot.start();
